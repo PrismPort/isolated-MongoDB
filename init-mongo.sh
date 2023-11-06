af441616eff944a4129c783a248de71e38bbedac
Marlin Jai <marlinjp@icloud.com>
2023-11-06 20:22:02 +0100

    added drop to repopulate on conatiner start

--- init-mongo.sh
+++ init-mongo.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 set -e
 
-mongoimport --host localhost --db TestDB --collection TestSchoolData --type json --file /sample-data.json --jsonArray
-mongoimport --host localhost --db TestDB --collection TestUserData --type json --file /sample-users.json --jsonArray
+# Initiate MongoDB command-line shell with admin authentication
+mongo -u "$MONGO_INITDB_ROOT_USERNAME" -p "$MONGO_INITDB_ROOT_PASSWORD" --authenticationDatabase admin <<EOF
+# Drop the TestDB if it exists
+use schoolTestDB
+db.dropDatabase();
+EOF
+
+# Import the data into the respective collections
+mongoimport --host localhost --db TestDB --collection TestSchoolData --type json --file /sample-data.json --jsonArray --authenticationDatabase admin -u "$MONGO_INITDB_ROOT_USERNAME" -p "$MONGO_INITDB_ROOT_PASSWORD"
+mongoimport --host localhost --db TestDB --collection TestUserData --type json --file /sample-users.json --jsonArray --authenticationDatabase admin -u "$MONGO_INITDB_ROOT_USERNAME" -p "$MONGO_INITDB_ROOT_PASSWORD"
