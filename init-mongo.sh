ba00daf2fc9984914fc6e0aea0aad490d62437a5
Marlin Jai <marlinjp@icloud.com>
2023-11-06 20:46:44 +0100

    removed volumes

--- init-mongo.sh
+++ init-mongo.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 set -e
 
-# Initiate MongoDB command-line shell with admin authentication
-mongo -u "$MONGO_INITDB_ROOT_USERNAME" -p "$MONGO_INITDB_ROOT_PASSWORD" --authenticationDatabase admin <<EOF
-# Drop the TestDB if it exists
-use schoolTestDB
-db.dropDatabase();
-EOF
-
 # Import the data into the respective collections
 mongoimport --host localhost --db TestDB --collection TestSchoolData --type json --file /sample-data.json --jsonArray --authenticationDatabase admin -u "$MONGO_INITDB_ROOT_USERNAME" -p "$MONGO_INITDB_ROOT_PASSWORD"
 mongoimport --host localhost --db TestDB --collection TestUserData --type json --file /sample-users.json --jsonArray --authenticationDatabase admin -u "$MONGO_INITDB_ROOT_USERNAME" -p "$MONGO_INITDB_ROOT_PASSWORD"
