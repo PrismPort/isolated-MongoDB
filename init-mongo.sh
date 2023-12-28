a4549759df929d3ea9ac52994fb693d1ad179836
Marlin Jai <marlinjp@icloud.com>
2023-12-28 14:22:45 +0100

    added more sample data

--- init-mongo.sh
+++ init-mongo.sh
@@ -8,3 +8,6 @@ mongoimport --verbose --host localhost --db TestDB --collection TestSchoolData -
 echo "Importing TestUserData..."
 mongoimport --verbose --host localhost --db TestDB --collection TestUserData --type json --file /sample-users.json --jsonArray --authenticationDatabase admin -u "$MONGO_INITDB_ROOT_USERNAME" -p "$MONGO_INITDB_ROOT_PASSWORD" || echo "Failed to import TestUserData"
 
+echo "Importing TestSchoolData..."
+mongoimport --verbose --host localhost --db TestDB --collection SchoolDataExtended --type json --file /sample-school.json --jsonArray --authenticationDatabase admin -u "$MONGO_INITDB_ROOT_USERNAME" -p "$MONGO_INITDB_ROOT_PASSWORD" || echo "Failed to import TestUserData"
+
