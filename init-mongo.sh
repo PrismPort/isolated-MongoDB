bb21383f59d23e6bfca2a0cab18425ff05ca9136
Marlin Jai <marlinjp@icloud.com>
2024-02-01 16:22:13 +0100

    removed arrays

--- init-mongo.sh
+++ init-mongo.sh
@@ -14,17 +14,17 @@ mongoimport --verbose --host localhost --db UsersDB --collection TestUserData --
 echo "Importing profiles..."
 mongoimport --verbose --host localhost --db UsersDB --collection profiles --type json --file /profiles.json --jsonArray --authenticationDatabase admin -u "$MONGO_INITDB_ROOT_USERNAME" -p "$MONGO_INITDB_ROOT_PASSWORD" || echo "Failed to import profiles"
 
-echo "Importing books..."
-mongoimport --verbose --host localhost --db Books --collection books --type json --file /books.json --jsonArray --authenticationDatabase admin -u "$MONGO_INITDB_ROOT_USERNAME" -p "$MONGO_INITDB_ROOT_PASSWORD" || echo "Failed to import books"
+# echo "Importing books..."
+# mongoimport --verbose --host localhost --db Books --collection books --type json --file /books.json --jsonArray --authenticationDatabase admin -u "$MONGO_INITDB_ROOT_USERNAME" -p "$MONGO_INITDB_ROOT_PASSWORD" || echo "Failed to import books"
 
-echo "Importing books covers..."
-mongoimport --verbose --host localhost --db Books --collection covers --type json --file /covers.json --jsonArray --authenticationDatabase admin -u "$MONGO_INITDB_ROOT_USERNAME" -p "$MONGO_INITDB_ROOT_PASSWORD" || echo "Failed to import book covers"
+# echo "Importing books covers..."
+# mongoimport --verbose --host localhost --db Books --collection covers --type json --file /covers.json --jsonArray --authenticationDatabase admin -u "$MONGO_INITDB_ROOT_USERNAME" -p "$MONGO_INITDB_ROOT_PASSWORD" || echo "Failed to import book covers"
 
 echo "Importing restaurant data..."
 mongoimport --verbose --host localhost --db Restaurants --collection restaurantData --type json --file /restaurant.json --jsonArray --authenticationDatabase admin -u "$MONGO_INITDB_ROOT_USERNAME" -p "$MONGO_INITDB_ROOT_PASSWORD" || echo "Failed to import restaurantData"
 
-echo "Importing street trees..."
-mongoimport --verbose --host localhost --db StreetTrees --collection streetTrees --type json --file /StreetTrees.json --jsonArray --authenticationDatabase admin -u "$MONGO_INITDB_ROOT_USERNAME" -p "$MONGO_INITDB_ROOT_PASSWORD" || echo "Failed to import streetTrees"
+# echo "Importing street trees..."
+# mongoimport --verbose --host localhost --db StreetTrees --collection streetTrees --type json --file /StreetTrees.json --jsonArray --authenticationDatabase admin -u "$MONGO_INITDB_ROOT_USERNAME" -p "$MONGO_INITDB_ROOT_PASSWORD" || echo "Failed to import streetTrees"
 
 echo "Importing restaurant LotteryNumbers.."
 mongoimport --verbose --host localhost --db LotteryNumbers --collection LotteryNumbers --type json --file /LotteryNumbers.json --jsonArray --authenticationDatabase admin -u "$MONGO_INITDB_ROOT_USERNAME" -p "$MONGO_INITDB_ROOT_PASSWORD" || echo "Failed to import LotteryNumbers"
